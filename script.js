33f1d99181fc9c067d403d12fb5a8a361ef1785b
Gediminas Kubilius <gekubilius@gmail.com>
2022-01-18 13:48:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+var tabs = document.getElementsByClassName("tab");
+var buttons = document.getElementsByTagName("button");
+
+function changeTab(target, name){
+    for(var i = 0; i <  tabs.length; i++){
+        tabs[i].style.display = "none";
+        buttons[i].classList.remove("selected")
+        
+    }
+    target.currentTarget.classList.add("selected");
+    document.getElementById(name).style.display = "flex";
+    
+
+
+}
+
